95378de3aa36916162ac9602dd52ef5b298d3a01
electromind <electromind.work@gmail.com>
2019-01-29 18:31:22 +0200

    fix constants

--- constants.py
+++ constants.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
-import datetime
-class trade_interval:
+app_mode = 'debug'  # 'release', 'debug_no_auth', 'debug'
+testnet = '10.0.1.9'
+testnet2 = '10.0.1.5'
+mainnet = '109.104.178.163'
+port_main = 2511
+port_test = 2512
+
+
+def get_mode():
+    return app_mode
+
+
+def get_net():
+    if app_mode == 'release':
+        network = mainnet
+        port = port_main
+    else:
+        network = testnet
+        port = port_test
+    return network, port
+
+
+class TradeInterval:
     ONE_MINUTE = 60000,
     FIVE_MINUTES = 300000,
     HALF_HOUR = 1800000,
@@ -8,36 +29,8 @@ class trade_interval:
     SIX_HOURS = 21600000,
     DAY = 86400000
 
-print(datetime.datetime.now().timestamp())
 
-class pricing:
-
-    def __init__(self):
-        self._current = None
-        self._default = "center"
-
-    @property
-    def current_method(self):
-        return self._current
-
-    @current_method.setter
-    def current_method(self, method):
-        if not isinstance(method, str):
-            msg = f'Unknown pricing method type: {type(method)}'
-            print(msg)
-            raise TypeError(msg)
-
-        elif method != pricing_methods.CENTER:
-            msg = f'Unknown pricing method value: {str(method)}'
-            print(msg)
-            raise ValueError(msg)
-
-        else:
-            self._current = str(method)
-            print(f'Current pricing method: {self.current_method}')
-
-
-class pricing_methods:
+class PricingMethods:
     CENTER = 'center'
     BID = 'bid'
     ASK = 'ask'
